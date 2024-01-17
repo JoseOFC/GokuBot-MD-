da3e4fc88656fa3946dd6ee62d92fd6f7ca70e97
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-01-17 17:04:03 -0500

    Update rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -1,11 +1,11 @@
 import { createHash } from 'crypto'
 let handler = async function (m, { args }) {
-if (!args[0]) throw `*⚠️ INGRESE SU NÚMERO DR SÉRIE, SI NO SABE CUAL ES USE #myns*`
+if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] DEBE INGRESAR SU NÚMERO DE SÉRIE, SI NO SABE CUAL ES USE .myns*`
 let user = global.db.data.users[m.sender]
 let sn = createHash('md5').update(m.sender).digest('hex')
-if (args[0] !== sn) throw `*⚠️ VERIFIQUE QUE SU NÚMERO DE SÉRIE SEA CORRECTO*`
+if (args[0] !== sn) throw `*[❗𝐈𝐍𝐅𝐎❗] DEBE VERIFICAR QUE SU NÚMERO DE SÉRIE SEA CORRECTO*`
 user.registered = false
-m.reply(`*📇 USTED YA NO ESTÁ REGISTRADO*`)
+m.reply(`*📇 USTED YA NO ESTÁ REGISTRADO EN MI BASE DE DATOS*`)
 }
 handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <numero de serie>')
 handler.tags = ['rg']
