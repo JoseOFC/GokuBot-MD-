e5fb1ff0e70f6939da8fd500bad25ebded104555
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-01-17 18:51:15 -0500

    Update store.js

--- store.js
+++ store.js
@@ -20,10 +20,10 @@ global.rosas = fs.readFileSync('./storage/imagenes/rosas.jpg')
 global.refran = fs.readFileSync('./storage/imagenes/refran.jpg')
 global.linksjpg = fs.readFileSync('./storage/imagenes/Links.jpg')
 
-var img = 'https://telegra.ph/file/139a1dd3560cd4a6bffe9.jpg'; var img1 = 'https://telegra.ph/file/8b89a9bc901b84006d948.jpg'; var img3 = 'https://telegra.ph/file/13aa381988656db8ba059.jpg'; var img4 = 'https://telegra.ph/file/c48531dbfe2fe17093e4d.jpg'; var img5 = 'https://telegra.ph/file/d64e69dc4321125f1c42c.jpg'
-var img6 = 'https://telegra.ph/file/85c5071f1c2a3763ca278.jpg'
+var img = 'https://telegra.ph/file/b4e12bbe02bd3c0555f29.jpg'; var img1 = 'https://telegra.ph/file/b4e12bbe02bd3c0555f29.jpg'; var img3 = 'https://telegra.ph/file/b4e12bbe02bd3c0555f29.jpg'; var img4 = 'https://telegra.ph/file/b4e12bbe02bd3c0555f29.jpg'; var img5 = 'https://telegra.ph/file/b4e12bbe02bd3c0555f29.jpg'
+var img6 = 'https://telegra.ph/file/b4e12bbe02bd3c0555f29.jpg'
 //Navideñas
-var img2 = 'https://telegra.ph/file/b9b4821c3f78b7ce6d3aa.jpg'
+var img2 = 'https://telegra.ph/file/b4e12bbe02bd3c0555f29.jpg'
 
 global.sityImg = [img, img1, img2, img3, img4, img5, img6]
 
@@ -34,10 +34,9 @@ global.md = 'https://github.com/AzamiJs/CuriosityBot-MD'
 global.paypal = 'https://paypal.me/Azami19'
 global.git = 'https://github.com/AzamiJs'
 global.nn = 'https://chat.whatsapp.com/G9MpDVmJtbiIpItEZpg70s' //Grupo ofc
-global.nnn = 'https://chat.whatsapp.com/J1R402WH1N0Hdl3S0NDEYu' //Curiosity global
-global.nnnn = 'https://chat.whatsapp.com/KLpYFsitkgzJ15YQxZrBqO' //colaboración multi bots
-global.nnnnn = 'https://chat.whatsapp.com/E9qJfvlLjENKAxnhII9rao' //Colaboración CuriosityBot - NovaBot 
-global.nnnnnn = 'https://chat.whatsapp.com/KlRcEfl8snBEEOA0JXzw5A' //enlace curiosity
+global.nnn = 'https://chat.whatsapp.com/CedAfdPDr5cHkyvvw6nTAu' //Staff de GokuBot-MD |Solo personal autorisado|
+global.nnnn = 'https://chat.whatsapp.com/Gd85w1s6tKh5Nw4p490xBw' //colaboración GokuBot-MD - NexusBot-MD
+global.nnnnn = 'https://chat.whatsapp.com/KPYQmm3METq2lKuT6e3NbM' //Colaboración GokuBot-MD - DiabloBot-MD
 global.channel = 'https://whatsapp.com/channel/0029VaJL0xn0LKZL7FtiRs1e' //canal ofc
 global.email = 'thecuriositybot@gmail.com'
 global.linkSity = [yt, ig, md, paypal, git, channel]
