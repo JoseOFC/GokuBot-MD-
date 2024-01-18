22c4e535ff72a4c08d81d2c1c18ac73d891932e3
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-01-18 14:56:10 -0500

    Actualizar info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -2,9 +2,12 @@ let media = './storage/logos/Menu2.jpg'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let str = `*「 🌟 DONAR 🌟 」*
----------------------
+------------------------------------------
 💙 *Me Puedes Ayudar Compartiendo Mi Bot, Y Darle Una Estrella A Mi Repositorio Oficial* 🪁
----------------------
+------------------------------------------
+*🪁Puedes Donarme En Paypal*
+https://www.paypal.me/EnzitoOFC
+------------------------------------------
 👑 *Creador del bot*
 💬 wa.me/593939005387`
 
