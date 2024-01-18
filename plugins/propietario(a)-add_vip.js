75b1107efa662682fce719efee0c7d28ae0061b0
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-01-18 11:22:31 -0500

    Update propietario(a)-add_vip.js

--- plugins/propietario(a)-add_vip.js
+++ plugins/propietario(a)-add_vip.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
 let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
 else who = m.chat
-if (!who) throw `*⚠️ ETIQUETE A LA PERSONA QUE VA A SER USUARIO VIP\n\n💡 EJEMPLO\n*${usedPrefix + command} @tag*`
+if (!who) throw `*[❗𝐈𝐍𝐅𝐎❗] DEBE ETIQUETAR A LA PERSONA QUE VA A SER USUARIO VIP\n\n💡 EJEMPLO\n*${usedPrefix + command} @tag*`
 if (global.prems.includes(who.split`@`[0])) throw `*EL USUARIO YA ES VIP ✨*`
 global.prems.push(`${who.split`@`[0]}`)
 conn.reply(m.chat, `*@${who.split`@`[0]} AHORA ES USUARIO VIP. NO VA A TENER LÍMITES CON ${cb} 😏*`, m, {
