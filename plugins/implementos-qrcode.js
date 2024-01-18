14047df2cceac0be5b02a9062ab9d0585b6c4ab1
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-01-18 11:34:50 -0500

    Update implementos-qrcode.js

--- plugins/implementos-qrcode.js
+++ plugins/implementos-qrcode.js
@@ -2,8 +2,8 @@ import { toDataURL } from 'qrcode'
 
 var handler = async (m, { text, conn }) => {
 
-if (!text) throw `*⚠️ INGRESE EL TEXTO QUE QUIERE CONVERTIR EN CÓDIGO QR*`
-conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '*🧃 CÓDIGO QR*', m)
+if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] DEBE INGRESAR EL TEXTO QUE QUIERE CONVERTIR EN CÓDIGO QR*`
+conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '*🙈 CÓDIGO QR*', m)
 
 }
 handler.help = ['qrcode']
