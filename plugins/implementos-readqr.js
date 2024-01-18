85a98119ab7607b32021f8c58dde57e74aaf7917
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-01-18 11:35:57 -0500

    Update implementos-readqr.js

--- plugins/implementos-readqr.js
+++ plugins/implementos-readqr.js
@@ -5,13 +5,13 @@ var handler = async (m, {conn, text, usedPrefix, command}) => {
 
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw '*⚠️ RESPONDA A UNA IMAGEN*'
+if (!mime) throw '*[❗𝐈𝐍𝐅𝐎❗] TIENE QUE RESPONDER A UNA IMAGEN*'
 let img = await q.download?.()
 let url = await uploadImage(img)
 let anu = await fetch(`https://api.lolhuman.xyz/api/read-qr?apikey=${lolkeysapi}&img=${url}`)
 let json = await anu.json()
 
-await m.reply(`*🧃 El Texto del Codigo QR Es:* ${json.result}`)
+await m.reply(`*💫 El Texto del Codigo QR Es:* ${json.result}`)
   
 }
 handler.help = ['readqr']
