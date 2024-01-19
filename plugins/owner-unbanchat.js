c8f7ed0b526bdb311d28534f89c2baae78109e45
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-01-19 12:45:35 -0500

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = false
-m.reply('*[💎] ESTE CHAT FUE  DESBANEADO [💎]*\n\n*🍓 ESTE CHAT YA TIENE PERMITIDO USARME🍓*')
+m.reply('*_[💎] ESTE HERMOSO CHAT FUE DESBANEADO POR UN STAFF DEL GRUPO [💎]_*\n\n *_🍓 ESTE HERMOSO CHAT YA TIENE PERMITIDO USAR A GokuBot-MD 🍓_*')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
