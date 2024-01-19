ae72a2256d971ff673734163cad7ed2f7a5226ba
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-01-19 12:47:09 -0500

    Update owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -1,12 +1,12 @@
 let handler = async (m, {conn, text}) => {
-  if (!text) throw "*⚠️ INGRESA EL @tag DE ALGUN USUARIO*"
+  if (!text) throw "*[❗𝐈𝐍𝐅𝐎❗] TIENE QUE INGRESAR EL @tag DE ALGUN USUARIO*"
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw "*⚠️ INGRESA EL @tag DE ALGUN USUARIO*"
+  if (!who) throw "*[❗𝐈𝐍𝐅𝐎❗] DEBE INGRESAR EL @tag DE ALGUN USUARIO*"
   let users = global.db.data.users
   users[who].banned = false
-  conn.reply(m.chat, `*⏤͟͟͞͞🍓ESTE USUARIO FUE DESBANEADO CON ÉXITO*\n*[🌟] YA PUEDE USAR CURIOSITY BOT-MD*`, m)}
+  conn.reply(m.chat, `*⏤͟͟͞͞🍓ESTE USUARIO FUE DESBANEADO CON ÉXITO*\n*[🌟] YA PUEDE USAR GOKU BOT-MD*`, m)}
 handler.help = ["unbanuser"]
 handler.tags = ["owner"]
 handler.command = /^unbanuser$/i
