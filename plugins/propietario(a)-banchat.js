9be6a89028f97693ab5010cb04da69bd7b83e60c
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-01-19 12:53:47 -0500

    Update propietario(a)-banchat.js

--- plugins/propietario(a)-banchat.js
+++ plugins/propietario(a)-banchat.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = true
-m.reply(`*[🛑] ESTE CHAT FUE BANEADO [🛑]*\n\n*❌ESTE CHAT NO TIENE PERMISO PARA USARME ASTA QUE SEA DESBANEADO❌*`)
+m.reply(`*_[❗𝐈𝐍𝐅𝐎❗] ESTE HERMOSO CHAT FUE BANEADO_*\n\n*_[🚨]ESTE HERMOSO CHAT NO PUEDE USARME HASTA QUE UN SATFF LO DESBANEE[🚨]_*`)
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
